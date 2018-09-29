6f391b96ac24a0015ef77c1d3d4b2500c1608330
Louiso <Gransullca.25@gmail.com>
2018-09-29 01:17:25 -0500

    se imprime la lista

---
+++ keeper-client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import { temas } from './temas';
+
+class App extends Component {
+  renderTopicos(tema){
+    console.log(tema);
+    return tema.topicos.map((topico, index)=>{
+      return (
+      <div key = {index}>
+        { topico.titulo }
+      </div>)
+    });
+  }
+  renderTemas(){
+    return temas.Basico.map((tema, index)=>{
+      return(
+        <div key = {index}>
+          <h2>{ tema.titulo }</h2>
+          <p>
+            { this.renderTopicos(tema) }
+          </p>
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        { this.renderTemas() }
+      </div>
+    );
+  }
+}
+
+export default App;
