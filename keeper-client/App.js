0c75cae585237391b08da8c356be06283ba469e9
Louiso <Gransullca.25@gmail.com>
2018-10-08 17:10:01 -0500

    listo vista login, pero falta dejarlo mas limpio

--- keeper-client/App.js
+++ keeper-client/App.js
@@ -1,47 +1,86 @@
 import React from 'react';
-import { StyleSheet, View , Text, TouchableHighlight} from 'react-native';
-import { Container, Form, Item, Label, Input } from 'native-base';
+import { 
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Keyboard,
+  ImageBackground,
+  TouchableNativeFeedback
+} from 'react-native';
+import { Container, Form, Item, Label, Input, View } from 'native-base';
+
+const DissmissKeyBoard = ({ children }) => (
+  <TouchableWithoutFeedback onPress = { () => { console.log('Presionado'); Keyboard.dismiss()} }>
+    { children }
+  </TouchableWithoutFeedback>
+)
+
 
 export default class App extends React.Component {
   
   render() {
     return (
-      <Container style={ styles.Container }>
-        <Form style = { styles.Form }>
-          <Item floatingLabel last style = { styles.Item }>
-            <Label style = {styles.ItemText } >Usuario</Label>
-            <Input />
-          </Item>
-          <Item floatingLabel last style = { styles.Item }>
-            <Label style = {styles.ItemText }>Contraseña</Label>
-            <Input />
-          </Item>
-          <TouchableHighlight
-            underlayColor = '#bf360c'
-            onPress = { ()=> console.log('Login Presionado')}
-            style = { styles.ButtonLogin }>
-            <Text style = {styles.ButtonLoginText } >Iniciar Sesion</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress = { ()=> console.log('Registro Presionado')}>
-            <Text style = { styles.RegisterText}> Registrate </Text>
-          </TouchableHighlight>
-        </Form>
-      </Container>
+      <DissmissKeyBoard>  
+          <Container>
+            <ImageBackground source={require('./assets/img/fondo.png')} style = { styles.Container }>
+              <View>
+                <Text style = { styles.Title }>KEEPER</Text>
+                <Text style = { styles.Subtitle }>PROXIMAMENTE</Text>
+                <Form style = { styles.Form }>
+                  <Item floatingLabel last style = { styles.Item }>
+                    <Label style = {styles.ItemText } >Usuario</Label>
+                    <Input/>
+                  </Item>
+                  <Item floatingLabel last style = { styles.Item }>
+                    <Label style = {styles.ItemText }>Contraseña</Label>
+                    <Input/>
+                  </Item>
+                  <TouchableHighlight
+                    underlayColor = '#bf360c'
+                    onPress = { ()=> console.log('Login Presionado')}
+                    style = { styles.ButtonLogin }>
+                    <Text style = {styles.ButtonLoginText } >Iniciar Sesion</Text>
+                  </TouchableHighlight>
+                  <TouchableNativeFeedback
+                    onPress = { ()=> console.log('Registro Presionado')}>
+                    <Text style = { styles.RegisterText}> Registrate </Text>
+                  </TouchableNativeFeedback>
+                </Form>
+              </View>
+            </ImageBackground>
+          </Container>
+        </DissmissKeyBoard >
+
     );
   }
 }
 
 const styles = StyleSheet.create({
   Container:{
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    opacity: 1
   },
   Form: {
     marginTop: 180,
     width: '55%',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  Title:{
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    letterSpacing: 8,
+    opacity: 0.8
+  },
+  Subtitle:{
+    textAlign: 'center',
+    fontSize: 11,
+    opacity: 0.5
   },
   Item:{
     backgroundColor: 'white',
